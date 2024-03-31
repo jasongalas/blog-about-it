5a70374ba002055e767ef5db439203c950df3e40
jasongalas <jasongalas@gmail.com>
2024-03-30 23:30:48 -0700

    Made various edits to the javascript and html files

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+/* Start by laying out the variables that are referencing the input lines in the
+HTML file to give us the ability to manipulate them. Make an array to store the blog posts in.*/
+
+
+const chosenName = document.querySelector("#name");
+const title = document.querySelector("#title");
+const content = document.querySelector("#content");
+const button = document.querySelector("#post");
+
+let posts = [];
+
+// Make a function that checks if there are posts in local storage. If there are, fill the post variable//
+
+function init(){
+    const blogPosts = JSON.parse(localStorage.getItem('posts'));
+
+    if (blogPosts !== null){
+    posts = blogPosts;
+    }
+}
+
+//Make a function that creates the blog posts, stores them, adds them to the array, and changes pages.
+
+function writeABlog(){
+    const blogPost = {
+        userName: chosenName.value.trim(),
+        blogTitle: title.value.trim(),
+        blogContent: content.value.trim()
+    }
+
+    posts.unshift(blogPost);
+    blogPost.value = "";
+
+    localStorage.setItem(`posts`, JSON.stringify(posts));
+
+    window.location.href="blog.html/"
+}
+
+//Make a function that writes the blog posts, parsing the JSON.
+
+function postABlog(){
+    const blogPosts = JSON.parse(localStorage.getItem('posts'));
+
+    for (let blog = 0; blog < posts.length; i++) {
+        const todo = todos[i];
+    
+        const li = document.createElement('li');
+        li.textContent = blog;
+        li.setAttribute('data-index', blog);
+    
+        li.appendChild(button);
+        blogPosts.appendChild(li);
+    }
+}
+
+//Make an event that triggers the function, and double checks that the fields are not blank.
+
+button.addEventListener('click', function (event) {;
+    event.preventDefault;
+
+    if (chosenName.value === '' || title.value === '' || content.value === '') {
+        alert("Please fill in all fields!");
+        
+    return;
+    }
+
+    writeABlog();
+    postABlog();
+    
+});
+
+init();
